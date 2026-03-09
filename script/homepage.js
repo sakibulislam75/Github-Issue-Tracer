711c76b338cd6e53936f7886acb85d3106609a76
sakibulislam75 <sakibulislam1623@gmail.com>
2026-03-09 16:28:33 +0600

    modify all the code and add modal structure and modul functionality still work in progress..

--- script/homepage.js
+++ script/homepage.js
@@ -1,4 +1,4 @@
-//toggle-button
+// toggle-button
 function toggleStyle(id) {
     const all = document.getElementById('all');
     const open = document.getElementById('open');
@@ -15,29 +15,33 @@ function toggleStyle(id) {
     const clickedBtn = document.getElementById(id);
     clickedBtn.classList.remove('bg-gray-100');
     clickedBtn.classList.add('btn-primary', 'text-white');
-
 }
 
-async function loadAll() {
 
+//api call
+async function loadAll() {
     const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const dt = await response.json();
     displayCard(dt.data);
 }
 
 document.getElementById('all').addEventListener('click', loadAll);
-
 loadAll();
 
+//display data on the container
 const displayCard = (allData) => {
         const container = document.getElementById('container');
         container.innerHTML = '';
         for (let data of allData) {
+            const borderColor = data.status === 'open' ? 'green' : data.status === 'medium' ? 'green' : 'blue';
             const div = document.createElement('div');
-            div.innerHTML = `<div class="bg-white shadow-md rounded-2xl border p-4 flex flex-col gap-3">
+            div.classList.add('cursor-pointer');
+            div.onclick = () => openModal(data);
+
+            div.innerHTML = `<div style="border-top: 4px solid ${borderColor}" class="bg-white shadow-md rounded-2xl border p-4 flex flex-col gap-3">
 
 <div class="flex justify-end">
-  <span class="text-xs font-semibold rounded-full px-3 py-1 border bg-amber-200 rounded-full px-3 py-1 ">${data.priority}</span>
+  <span class="text-xs font-semibold rounded-full px-3 py-1 border bg-amber-200">${data.priority}</span>
 </div>
 
   <h2 class="font-bold text-base">${data.title}</h2>
@@ -46,9 +50,7 @@ const displayCard = (allData) => {
 
   <div class="flex gap-2">
 ${data.labels.map(label => `
-  <span class="text-xs border rounded-full px-3 py-1">
-    ${label}
-  </span>
+  <span class="text-xs border rounded-full px-3 py-1">${label}</span>
 `).join('')}
   </div>
 
@@ -59,37 +61,40 @@ ${data.labels.map(label => `
     <p>${data.author}</p>
     <p>${data.createdAt}</p>
   </div>
-   <div class=" text-xs ">
+  <div class="text-xs">
     <p>${data.updatedAt}</p>
-  </div></div>
+  </div>
+</div>
 
 </div>`;
         container.appendChild(div);
     }
 
-   document.getElementById('total-issues').innerText = allData.length;
+    document.getElementById('total-issues').innerText = allData.length;
 }
+//open
 document.getElementById('open').addEventListener('click', async function() {
+    toggleStyle('open');
     const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const dt = await response.json();
     const openData = dt.data.filter(item => item.status === 'open');
     displayCard(openData);
-    document.getElementById('total-issues').innerText = ` ${openData.length}`;
 });
 
+//close
 document.getElementById('close').addEventListener('click', async function() {
+    toggleStyle('close');
     const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const dt = await response.json();
     const closeData = dt.data.filter(item => item.status === 'closed');
     displayCard(closeData);
-    document.getElementById('total-issues').innerText = ` ${closeData.length}`;
 });
 
-
-//search using title
+//search
 document.getElementById('issues').addEventListener('click', async function() {
     const searchText = document.getElementById('search').value.trim();
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`);
     const data = await res.json();
     displayCard(data.data);
+    document.getElementById('total-issues').innerText = data.data.length;
 });
