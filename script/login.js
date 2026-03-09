22e9b24bd97ab6cf29ad906a8264356567108421
sakibulislam75 <sakibulislam1623@gmail.com>
2026-03-09 17:45:50 +0600

    fix all issues and add spinner and solves responsive issues

--- script/login.js
+++ script/login.js
@@ -1,11 +1,18 @@
 document.getElementById('login').addEventListener('click', function() {
-    const name = document.getElementById('userName');
-    const userName = name.value.trim();
-    const pass = document.getElementById('password');
-    const password = pass.value.trim();
+    const userName = document.getElementById('userName').value.trim();
+    const password = document.getElementById('password').value.trim();
+
     if (userName === 'admin' && password === 'admin123') {
-        window.location.assign('./home.html');
+        document.body.innerHTML += `
+            <div class="fixed inset-0 bg-white/80 flex justify-center items-center z-50">
+                <span class="loading loading-bars loading-xl"></span>
+            </div>
+        `;
+
+        setTimeout(() => {
+            window.location.assign('./home.html');
+        }, 1500);
     } else {
         alert('Log-in Failed');
     }
-})
+});
