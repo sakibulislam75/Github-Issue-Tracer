2a2539898c67b1fa733eeecd34eb4d5fadd264cb
sakibulislam75 <sakibulislam1623@gmail.com>
2026-03-09 11:13:16 +0600

    add log in page

---
+++ script/login.js
@@ -0,0 +1,11 @@
+document.getElementById('login').addEventListener('click', function() {
+    const name = document.getElementById('userName');
+    const userName = name.value.trim();
+    const pass = document.getElementById('password');
+    const password = pass.value.trim();
+    if (userName === 'admin' && password === 'admin123') {
+        window.location.assign('./home.html');
+    } else {
+        alert('Log-in Failed');
+    }
+})
